44b5876019bd7eaf9f6cbbad6e85be47535187f5
TheKaiser8 <stefano.vanzini8@gmail.com>
2022-11-07 16:40:23 +0100

    Aggiunge MILESTONE 0: array di oggetti (ourTeam)

--- js/main.js
+++ js/main.js
@@ -11,9 +11,6 @@
 // Scott Estrada	Developer	scott-estrada-developer.jpg
 // Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
 
-// MILESTONE 0:
-// Creare l’array di oggetti con le informazioni fornite.
-
 // MILESTONE 1:
 // Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
 
@@ -32,3 +29,38 @@
 /*------------------
     MAIN
 --------------------*/
+// MILESTONE 0:
+// Creare l’array di oggetti con le informazioni fornite.
+// creo un ARRAY di OBJECTS che rappresentano ogni membro del team
+const ourTeam = [
+    {
+        fullName: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        fullName: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        fullName: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        fullName: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        fullName: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        fullName: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    }
+]
